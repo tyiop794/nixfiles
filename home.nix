85d7d4ec651edcbe64ef1d1888477a91b307f736
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-05-17 13:54:38 -0500

    2024-05-17--13:54:38

--- home.nix
+++ home.nix
@@ -47,6 +47,21 @@
 		];
 	};
   };
+  programs.fish = {
+  	enable = true;
+	shellInit = {
+	    ''
+	       if status is-interactive
+	            # stuff
+	       end
+
+	       set fish_greeting
+	       set -g fish_key_bindings fish_vi_key_bindings
+
+	       fish_add_path /home/kamilyousuf/nixfiles/scripts
+	    ''
+	};
+  }
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
