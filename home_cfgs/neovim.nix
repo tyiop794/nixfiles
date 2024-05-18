d0607c4b78396c8a80793ae5ccf05aea5f04c73e
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-05-17 22:27:13 -0500

    2024-05-17--22:27:13

--- home_cfgs/neovim.nix
+++ home_cfgs/neovim.nix
@@ -1,9 +1,15 @@
-{config, inputs, ... }:
+{config, inputs, pkgs, ... }:
 {
   # My neovim configuration
   # (will be more complex later)
   programs.neovim = {
   	enable = true;
   	defaultEditor = true;
+	plugins = with pkgs.vimPlugins; [
+	   nvim-lspconfig
+	   nvim-treesitter.withAllGrammars
+	   plenary-nvim
+	   gruvbox
+	];
   };
 }
