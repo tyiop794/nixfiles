bbf9590bf752cd60a87148cc99e0f54e3a8d561a
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-05-19 21:19:04 -0600

    2024-05-19--21:19:04

---
+++ devs/latex/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake with rust and cargo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let 
+  system = "x86_64-linux";
+  in {
+     devShells."${system}".default = let
+       pkgs = import nixpkgs {
+         inherit system;
+       };
+     in pkgs.mkShell {
+       packages = with pkgs; [
+          texlive.combined.scheme-full
+       ];
+       shellHook = "
+       exec fish
+       ";
+  };
+};
+}
