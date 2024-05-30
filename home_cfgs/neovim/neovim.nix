2fc5da9aeb5e3da47dd0625b4feafc574a5106f2
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-05-29 20:42:12 -0600

    2024-05-29--20:42:12

--- home_cfgs/neovim/neovim.nix
+++ home_cfgs/neovim/neovim.nix
@@ -23,12 +23,15 @@ in
 	vim.cmd('set expandtab sw=4 sts=4')
         vim.cmd('set number')
         vim.cmd('set relativenumber')
+        vim.cmd('packadd! nvim-tree.lua')
         -- disable netrw at the very start of your init.lua
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
 
         -- optionally enable 24-bit colour
         vim.opt.termguicolors = true
+        -- enable nvim-tree
+        require(\"nvim-tree\").setup()
 	";
   };
 }
