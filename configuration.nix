b3e4eb1055e3dbadcf81cae9cde71fb4e5491303
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-06-02 18:26:35 -0600

    2024-06-02--18:26:35

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./firejail.nix
       inputs.home-manager.nixosModules.home-manager
     ];
 
