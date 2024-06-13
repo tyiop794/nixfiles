78643d4a860211973924ed41d867b8cafda525b3
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-06-12 18:45:56 -0600

    2024-06-12--18:45:56

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,10 @@
     };
   };
   networking.hostName = "tardis"; # Define your hostname.
+
+  # Enable fwupd
+  services.fwupd.enable = true;
+
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
