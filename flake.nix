59bb864410c4d752c9d939a7756dce642ef300f6
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-10-20 15:04:24 -0500

    2024-10-20--15:04:24

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs = {
     # nixos-unstable repository
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
+    # nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
     home-manager = {
        url = "github:nix-community/home-manager/master";
        inputs.nixpkgs.follows = "nixpkgs";
