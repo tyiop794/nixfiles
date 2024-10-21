1db4cac641149516994864808fe87aed26e9a52f
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-10-21 12:49:46 -0500

    2024-10-21--12:49:46

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # nixos-unstable repository
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     # nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
+    # nix-flatpak.url = "github:gmodena/nix-flatpak";
     # home-manager = {
     #    url = "github:nix-community/home-manager/master";
     #    inputs.nixpkgs.follows = "nixpkgs";
@@ -35,7 +35,7 @@
         specialArgs = { inherit inputs outputs; };
 	modules = [
             lix-module.nixosModules.default
-            nix-flatpak.nixosModules.nix-flatpak
+            # nix-flatpak.nixosModules.nix-flatpak
 	   ./virtualisation.nix
 	   ./configuration.nix
            # ./firejail.nix
