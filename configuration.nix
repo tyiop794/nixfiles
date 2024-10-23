36e653a9550c5a1119ea8def43e6b713f87a9880
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-10-23 16:34:56 -0500

    2024-10-23--16:34:56: "get tlp to work (again)"

--- configuration.nix
+++ configuration.nix
@@ -245,8 +245,8 @@
   services.tlp = {
     enable = true;
     settings = {
-        PLATFORM_PROFILE_ON_AC = balanced;
-        PLATFORM_PROFILE_ON_BAT = low-power;
+        PLATFORM_PROFILE_ON_AC = "balanced";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
     };
   };
   services.power-profiles-daemon.enable = false;
