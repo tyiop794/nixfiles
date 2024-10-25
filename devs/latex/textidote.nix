f9e57906a7df24e77ce8a1489bbb80d3b4f228ff
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-10-25 00:00:08 -0500

    2024-10-25--00:00:08: "home-manager: update"

---
+++ devs/latex/textidote.nix
@@ -0,0 +1,38 @@
+{ lib, stdenv, fetchurl, jdk }:
+
+# TODO: fill this out to package textidote
+stdenv.mkDerivation rec {
+  pname = "your-java-program";
+  version = "1.0.0";
+
+  # Specify the source of the JAR file
+  src = fetchurl {
+    url = "https://example.com/your-java-program-${version}.jar";
+    sha256 = "sha256-hash-of-the-jar";
+  };
+
+  # The Java runtime environment needed to run the program
+  buildInputs = [ jdk ];
+
+  # Specify the output binary directory
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/share/java
+    cp $src $out/share/java/your-java-program.jar
+
+    # Create a wrapper script to run the jar file
+    cat > $out/bin/your-java-program <<EOF
+    #!${stdenv.shell}
+    exec ${jdk}/bin/java -jar $out/share/java/your-java-program.jar "\$@"
+    EOF
+
+    chmod +x $out/bin/your-java-program
+  '';
+
+  meta = with lib; {
+    description = "Your Java program packaged in Nix";
+    license = licenses.mit; # Adjust as appropriate
+    platforms = platforms.all;
+  };
+}
+
