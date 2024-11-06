a51b3ff94129237267811990836bb7e927ddb6da
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-11-05 18:26:14 -0600

    2024-11-05--18:26:14: "home-manager: update"

--- hm-kamilyousuf/flake.nix
+++ hm-kamilyousuf/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
     firefox-addons = {
        url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
        inputs.nixpkgs.follows = "nixpkgs";
@@ -37,7 +37,7 @@
         # the path to your home.nix.
         modules = [ 
             ./home.nix 
-            # nix-flatpak.homeManagerModules.nix-flatpak
+            nix-flatpak.homeManagerModules.nix-flatpak
         ];
 
         # Optionally use extraSpecialArgs
