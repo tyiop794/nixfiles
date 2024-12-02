09be6dc7964a1ea89db962f7bf378d95183c0298
Kamil Yousuf <kamilyousuf0712@gmail.com>
2024-12-01 20:12:23 -0600

    2024-12-01--20:12:23

--- configuration.nix
+++ configuration.nix
@@ -143,7 +143,7 @@
   # Install fonts system-wide
   fonts.packages = with pkgs; [
       noto-fonts
-      noto-fonts-cjk
+      noto-fonts-cjk-sans
       noto-fonts-emoji
       liberation_ttf
       fira-code
