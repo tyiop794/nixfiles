ea6d6ca2f7472885408ca42ae8cee4af9d9bac63
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-01-15 13:21:17 -0600

    2025-01-15--13:21:17

--- box/packages.nix
+++ box/packages.nix
@@ -3,4 +3,10 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  # environment.systemPackages = with pkgs; [
+  #   # partitionmanager
+  # ];
+
+  programs.partition-manager.enable = true;
+
 }
--- hm-kamilyousuf/home_pkgs/graphics.nix
+++ hm-kamilyousuf/home_pkgs/graphics.nix
@@ -4,5 +4,6 @@
         "com.github.finefindus.eyedropper"
         "org.inkscape.Inkscape"
         "org.gimp.GIMP"
+        "org.kde.kdenlive"
     ];
 }
