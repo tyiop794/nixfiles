3ac3a4ce450e7f79e821236818caf394519d65c2
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-01-16 02:16:57 -0600

    2025-01-16--02:16:57

--- box/packages.nix
+++ box/packages.nix
@@ -3,9 +3,9 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # environment.systemPackages = with pkgs; [
-  #   # partitionmanager
-  # ];
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+  ];
 
   programs.partition-manager.enable = true;
   programs.dconf.enable = true;
