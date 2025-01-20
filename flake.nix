e6f5d5c116c434e519570ea6b81280bea62cc049
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-01-19 21:29:18 -0600

    2025-01-19--21:29:18: "home-manager: update"

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
        inputs.nixpkgs.follows = "nixpkgs";
     };
     lix-module = {
-       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
        inputs.nixpkgs.follows = "nixpkgs";
     };
     # flatpak_nixpkgs = {
