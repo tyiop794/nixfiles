b9bee33a4492e22774bd036f7e8b896678baa68b
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-01-21 00:41:01 -0600

    2025-01-21--00:41:01

--- box/packages.nix
+++ box/packages.nix
@@ -3,6 +3,7 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  programs.kdeconnect.enable = true;
   environment.systemPackages = with pkgs; [
     gnome-tweaks
   ];
