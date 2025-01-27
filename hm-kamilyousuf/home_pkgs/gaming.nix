e0450bcf0b3bab8f9d4ccd8981b9e018f5003f7e
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-01-27 00:40:55 -0600

    2025-01-27--00:40:55

--- hm-kamilyousuf/home_pkgs/gaming.nix
+++ hm-kamilyousuf/home_pkgs/gaming.nix
@@ -3,17 +3,17 @@
     home.packages = with pkgs; [
      gamescope
     ];
-    services.flatpak.packages = [
-        "org.duckstation.DuckStation"
-        "io.github.simple64.simple64"
-        "net.pcsx2.PCSX2"
-        "dev.ares.ares"
-        "net.lutris.Lutris"
-        "org.flycast.Flycast"
-        "com.usebottles.bottles"
-        "net.davidotek.pupgui2"
-        "org.libretro.RetroArch"
-        "org.DolphinEmu.dolphin-emu"
-        "com.github.k4zmu2a.spacecadetpinball"
-    ];
+    # services.flatpak.packages = [
+    #     "org.duckstation.DuckStation"
+    #     "io.github.simple64.simple64"
+    #     "net.pcsx2.PCSX2"
+    #     "dev.ares.ares"
+    #     "net.lutris.Lutris"
+    #     "org.flycast.Flycast"
+    #     "com.usebottles.bottles"
+    #     "net.davidotek.pupgui2"
+    #     "org.libretro.RetroArch"
+    #     "org.DolphinEmu.dolphin-emu"
+    #     "com.github.k4zmu2a.spacecadetpinball"
+    # ];
 }
