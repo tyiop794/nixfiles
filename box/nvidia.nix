44492c51193a871872e0d7befcbeabef81095c0d
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-01-30 18:07:29 -0600

    2025-01-30--18:07:29

--- box/nvidia.nix
+++ box/nvidia.nix
@@ -14,6 +14,7 @@
         nvidia-container-toolkit
     ];
 
+    boot.kernelParams = ["nvidia.NVreg_PreserveVideoMemoryAllocations=1"];
     hardware.nvidia = {
         modesetting.enable = true;
         open = false;
--- hm-kamilyousuf/home_cfgs/bash.nix
+++ hm-kamilyousuf/home_cfgs/bash.nix
@@ -5,6 +5,7 @@
         bashrcExtra = ''
             set -o vi
             export PATH=$PATH:/home/kamilyousuf/nixfiles/scripts
+            export MOZ_DISABLE_RDD_SANDBOX=1
         '';
     };
 
