9171749579efe8e3478fd58be365fa4b4634219b
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-02-13 00:46:19 -0600

    2025-02-13--00:46:19

--- hm-kamilyousuf/home_cfgs/ssh.nix
+++ hm-kamilyousuf/home_cfgs/ssh.nix
@@ -6,6 +6,9 @@
             Host derecho
                 HostName derecho.hpc.ucar.edu
                 User kyousuf
+            Host lotus
+                HostName lotus.arc.rhodes.edu
+                User youka-25
         '';
     };
 }
