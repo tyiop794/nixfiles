31bdd2a975adf08018d8ccce1468afa871e7f6aa
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-02-19 21:49:55 -0600

    2025-02-19--21:49:55

--- hm-kamilyousuf/home_pkgs/internet.nix
+++ hm-kamilyousuf/home_pkgs/internet.nix
@@ -5,5 +5,6 @@
         "io.github.ungoogled_software.ungoogled_chromium"
         "org.qbittorrent.qBittorrent"
         "org.torproject.torbrowser-launcher"
+        "com.getpostman.Postman"
     ];
 }
