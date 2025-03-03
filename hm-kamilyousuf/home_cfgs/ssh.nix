143eabf3e12dc765ddd6a0902e7b2625f7527091
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-03-02 22:18:26 -0600

    2025-03-02--22:18:26

--- hm-kamilyousuf/home_cfgs/ssh.nix
+++ hm-kamilyousuf/home_cfgs/ssh.nix
@@ -9,6 +9,9 @@
             Host lotus
                 HostName lotus.arc.rhodes.edu
                 User youka-25
+            Host box
+                HostName 10.20.45.234
+                User kamilyousuf
         '';
     };
 }
