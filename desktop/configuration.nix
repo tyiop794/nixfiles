bebfc3f39e4f99ae330081c47d9c70a3753c7a1f
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-03-18 10:23:23 -0500

    2025-03-18--10:23:23

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -23,8 +23,12 @@
   services.fwupd.enable = true;
 
   # enable usbmuxd
-  services.usbmuxd.enable = true;
+  services.usbmuxd = {
+    enable = true;
+    package = pkgs.usbmuxd2;
+  };
 
+  services.avahi.enable = true;
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
