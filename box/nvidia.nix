1703f5216434e1601af80ffc6cf6c94b8011f59e
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-04-21 15:05:59 -0500

    Using open-source nvidia driver

--- box/nvidia.nix
+++ box/nvidia.nix
@@ -16,7 +16,7 @@
 
     hardware.nvidia = {
         modesetting.enable = true;
-        open = false;
+        open = true;
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
