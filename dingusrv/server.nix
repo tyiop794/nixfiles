0be938754a0198a75e1f50b5c63a0b9d5ae67cbb
Kamil Yousuf <kamilyousuf0712@gmail.com>
2025-07-21 14:03:44 -0500

    what's updated here?

--- dingusrv/server.nix
+++ dingusrv/server.nix
@@ -4,7 +4,7 @@
     imports = [
     # TODO: make this stuffs work
         # ./sql.nix
-        # ./nextcloud.nix
+        #./nextcloud.nix
     ];
     networking.firewall = {
         allowedTCPPorts = [ 80 443 ];
